1b32d8b5d0cc10da5f83cf2112554d026ec57470
ALIM <alymict@gmail.com>
2019-12-11 00:17:11 +0100

    sixth commit

--- src/Packages/StorePage/index.js
+++ src/Packages/StorePage/index.js
@@ -12,7 +12,7 @@ export class Store extends Component {
     render() {
         return (
             <section className="corpus_store">
-                <section className="section_gauche">
+                <section className="section_gauche_store">
 
                     <div className="entete">
                         <div className="titre">
@@ -117,7 +117,7 @@ export class Store extends Component {
                 </section>
 
 
-                <section className="section_droite">
+                <section className="section_droite_store">
 
                     <div className="maps">
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15923.444342583682!2d11.479809528278256!3d3.8399832553066995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x108bcfcfba91be3b%3A0x3eb618bb8fc3a303!2sBiyem-Assi%2C%20Yaound%C3%A9!5e0!3m2!1sfr!2scm!4v1573252016479!5m2!1sfr!2scm" width="600" height="450" frameborder="0" style={{ "border": "0" }} allowfullscreen="" title="Maps"></iframe>
