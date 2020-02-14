47141ed8a009b681f56f07d9f9450aa311f90494
ALIM <alymict@gmail.com>
2020-02-15 00:59:03 +0100

    Makissy 1.0.0

--- src/Packages/EditStore/index.js
+++ src/Packages/EditStore/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-// import './EditStore.scss';
+import './EditStore.scss';
 
 export default class EditStore extends Component {
     render() {
