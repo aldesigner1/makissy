a6d73d1a62ddf0fc9c78b0ae4268ce654ab09f1c
aldesigners1 <aldcorp.d1@gmail.com>
2020-06-23 08:36:11 +0100

    Makissy 1.0.3

--- src/Packages/HomePage/index.js
+++ src/Packages/HomePage/index.js
@@ -28,7 +28,7 @@ export default class HomePage extends Component {
                         following: "Suivi...",
                         prix: "--",
                         devise: " XAF",
-                        srcImage: "/Assets/images/test/icorp.gif",
+                        srcImage: "/Assets/images/test/icorp2.gif",
                         profilePic: "Assets/icons/IMG_20200114_153834_752.jpg",
                         srcBlur: "assets/images/Icons Animation Inspiration.gif",
                         delai: "Aujourd'hui",
@@ -283,7 +283,7 @@ export default class HomePage extends Component {
                 {
                     nomUtil: "Metys_Vision",
                     categorie: "job",
-                    titre: "Formation - Community Manager",
+                    titre: "Formation - CommunityManager",
                     following: "Suivre",
                     prix: "55 000",
                     devise: " XAF",
